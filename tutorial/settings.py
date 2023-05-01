bc59ef572c753173715eb609aef272d70bcf9d11
Tomihiro Toyozaki <tomihirotoyozaki@TomihironoMacBook-Pro.local>
2023-05-01 11:51:41 +0900

    sqlみるためにロギングレベルをDEBUGに

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -124,3 +124,20 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
